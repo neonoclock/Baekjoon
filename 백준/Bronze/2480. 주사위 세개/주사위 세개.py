093883394f124d9aabab856d6d80d1b2a0cee4b4
Hyewon Ahn <neonoclock213@naver.com>
2026-03-31 15:35:51 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+# Write your code here
+# A, B = map(int, input().split())
+# print(A + B)
+
+
+a, b, c = map(int, input().split())
+
+if a == b == c:
+    print(10000 + a * 1000)
+elif a == b or a == c:
+    print(1000 + a * 100)
+elif b == c:
+    print(1000 + b * 100)
+else:
+    print(max(a, b, c) * 100)
