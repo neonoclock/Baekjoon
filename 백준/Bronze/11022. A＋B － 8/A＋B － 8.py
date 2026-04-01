40d3be3d5f1d1c52c9cc0090f2e2fa5293e22b95
Hyewon Ahn <neonoclock213@naver.com>
2026-04-01 15:02:02 +0900

    [Bronze V] Title: A+B - 8, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+# Write your code here
+# A, B = map(int, input().split())
+# print(A + B)
+
+T = int(input())
+
+for i in range(1, T+1):
+    A, B = map(int, input().split())
+    C = A+B
+    print(f"Case #{i}: {A} + {B} = {C}")
