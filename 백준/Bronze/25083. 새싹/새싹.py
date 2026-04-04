d29d7601872152d3827dbcd327be1c3fe8b4d4a5
Hyewon Ahn <neonoclock213@naver.com>
2026-04-04 23:30:23 +0900

    [Bronze V] Title: 새싹, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+# Write your code here
+# A, B = map(int, input().split())
+# print(A + B)
+
+
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \. \". L_r'")
+print("   `~\/")
+print("      |")
+print("      |")
